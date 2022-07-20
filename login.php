aaded785a59111c40bf7d361a708c84cbcd32c12
Novia Bella <nobel.bella20@gmail.com>
2022-07-20 15:39:28 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <ul>
+<form action="admin.php" method="post"></form>
+    <li><label for="username">Username</label>
+    <input type="text" id="username">
+    </li>
+    <li>
+        <label for="password">Password</label>
+        <input type="password" id="password">
+    </li>
+    <li>
+        <button radius="15%" type="submit" >Login</button>
+    </li>
+    </ul>
+</body>
+</html>
