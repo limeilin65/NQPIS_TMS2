5424f5d1714abc244a23732d6219ba9469733fa7
fairuz <107167044+limeilin65@users.noreply.github.com>
2022-07-23 15:33:14 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,26 +1,56 @@
+<?php
+// cek apakah tombol submit sudah dipencet belum
+if (isset($_POST["submit"])) {
+
+    // cek username dan password
+    if ($_POST["username"] == "admin" && $_POST["password"] == "123") {
+        // jika benar, redirect ke halaman admin
+        header("Location: admin.php");
+        exit;
+    } else {
+        // jika salah tampilkan pesan kesalahan
+        $error = true;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>Login</title>
 </head>
+
 <body>
 
-    <h1>HALAMAN LOGIN</h1>
+    <h1>Login Admin</h1>
+    <?php if (isset($error)) : ?>
+        <p style="color: red; font-style:italic ">Username / Password Salah!</p>
+    <?php endif; ?>
     <ul>
-<form action="admin.php" method="post"></form>
-    <li><label for="username">Username</label>
-    <input type="text" id="username">
-    </li>
-    <li>
-        <label for="password">Password</label>
-        <input type="password" id="password">
-    </li>
-    <li>
-        <button radius="15%" type="submit" >Login</button>
-    </li>
+        <form action="" method="post">
+
+            <li>
+                <label for="username">Username :</label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password :</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <button type="submit" name="submit">Login</button>
+            </li>
+
+
+        </form>
     </ul>
+
+
+
 </body>
+
 </html>
