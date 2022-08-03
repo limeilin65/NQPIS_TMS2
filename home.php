0d2f5f61e5c4ed1431e01b69a56171e6cb71dc62
cho <107167499+cholifah83@users.noreply.github.com>
2022-08-03 14:24:20 +0700

    Update home.php

---
+++ home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NQ PIS_TMS2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="sylsheet" href="style.css"/>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-info shadow-lgfixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">NQ PIS_TMS2</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse text-right" id="navbarNav">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Data</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Persetujuan</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</nav>
+<div class="container-fluid banner"  style="bg-color:#fff">
+    <div class="container text-center">
+        <h1 class="display-3">NQ PIS_TMS2</h1>
+        <h2 class="display-5">NQ Permissions Information System</h3>
+        <p class="display-12">Sebuah sistem yang membantu santri, pengurus perizinan dan Pemangku Asrama dalam mengelola data. Saling terhubung satu sama lain akan membuat informasi yang ada selalu update secara real time.</p>
+       
+        <br><br>
+
+<br><br>
+        <h1 class="display-7 ">Our Team</h1>
+        <img src="" alt="">
+
+</div>
+</div>
+    <div class="p-3 mb-2 bg-body text-dark"></div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
